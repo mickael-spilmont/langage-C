7e305c38ddebda09e618c5eb9fbd75f47a140e5b
Mickael Spilmont <mickael.spilmont.fr@gmail.com>
2018-11-14 21:58:09 +0100

    tp4 en attente de correction

--- tp4/main_saisie.c
+++ tp4/main_saisie.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #include "tp4.h"
 
 int main(int argc, char* argv[]) {
 
   // Debug
-  printf("argc = %d\n", argc);
-
-  int z;
-  for (z = 0 ; z <= argc ; z++) {
-    printf("argv[%d] = %s\n", z, argv[z]);
-  }
+  // printf("argc = %d\n", argc);
+  //
+  // int z;
+  // for (z = 0 ; z <= argc ; z++) {
+  //   printf("argv[%d] = %s\n", z, argv[z]);
+  // }
   // Fin debug
 
-  if (argc == 2 && strcmp(argv[1], "-s") == 0) {
+  int f_saisie = 0;
+  int f_miroir = 0;
+  int f_erreur = 0;
+  int options;
+
+  while ((options = getopt(argc, argv, "sm")) != -1) {
+    switch (options) {
+    case 's':
+      f_saisie = 1;
+      break;
+    case 'm':
+      f_miroir = 1;
+      break;
+    default:
+    case '?':
+      f_erreur = 1;
+    }
+  }
+
+  if (!f_erreur && f_miroir && f_saisie && argv[argc - 1][0] == '-') {
+    printf("%s\n", miroir(saisie()));
+  }
+  else if (!f_erreur && f_saisie && argv[argc - 1][0] == '-') {
     printf("%s\n", saisie());
   }
-  else if (argc == 3 && strcmp(argv[1], "-m") == 0 && argv[2][0] != '-') {
-    printf("%s\n", miroir(argv[2]));
+  else if (!f_erreur && f_miroir && !f_saisie && argv[argc - 1][0] != '-') {
+    printf("%s\n", miroir(argv[argc - 1]));
   }
   else {
     printf("mauvaise utilisation\n");
+    return 1;
   }
 
   return 0;
